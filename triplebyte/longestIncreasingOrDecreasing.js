5997f4f7d490dcbfdea9ea12379a2de48c69863c
Ron Arbel <ronarbel1@gmail.com>
2019-03-25 00:27:09 -0700

    Add optimal solution

--- triplebyte/longestIncreasingOrDecreasing.js
+++ triplebyte/longestIncreasingOrDecreasing.js
@@ -35,5 +35,29 @@ const longestIncreasingOrDecreasing = (a) => {
   return Math.max(maxIncreasing, maxDecreasing);
 };
 
+// optimized O(1n)
+const longestIncreasingOrDecreasing2 = (a) => {
+  let max = 1;
+  let curInc = 1;
+  let curDec = 1;
+  for (let i = 1; i < a.length; i += 1) {
+    let cur = a[i];
+    let prev = a[i - 1];
+    if (cur > prev) {
+      curDec = 1;
+      curInc += 1;
+      max = Math.max(curInc, max);
+    } else if (cur < prev) {
+      curInc = 1;
+      curDec += 1;
+      max = Math.max(curDec, max);
+    } else {
+      curInc = 1;
+      curDec = 1;
+    }
+  }
+  return max;
+}
+
 const a1 = [1, 2, 3, 2, 4]; // 3 (increased for 3 days);
-console.log(longestIncreasingOrDecreasing(a1)); //
+console.log(longestIncreasingOrDecreasing2(a1)); // 3
