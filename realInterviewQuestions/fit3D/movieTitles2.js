c702c067849137abfa6925b1d3f37462b254338d
Ron Arbel <ronarbel1@gmail.com>
2019-04-11 11:27:22 -0700

    Recursively get all 13 movie titles, currently not returning

--- realInterviewQuestions/fit3D/movieTitles2.js
+++ realInterviewQuestions/fit3D/movieTitles2.js
@@ -1,30 +1,35 @@
 const https = require('https');
 
 const getMovieTitles = (substr) => {
-  const titles = [];
-  const getTitles = (substr, pageNumber) => {
-    https.get(`https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}`, (res) => {
+
+  const getTitles = (substr, pageNumber, titles) => {
+    https.get(`https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=${pageNumber}`, (res) => {
       console.log('statusCode:', res.statusCode);
       console.log('headers:', res.headers);
 
-      res.on('data', (d) => {
-        const data = JSON.parse(d);
-        data.data.forEach((movie) => {
+      res.on('data', (chunk) => {
+        const response = JSON.parse(chunk);
+        console.log(response);
+
+        // add retrieved titles to titles array.
+        response.data.forEach((movie) => {
           titles.push(movie.Title);
         });
 
-        console.log(titles);
+        // retrieve next page if more pages, else return gathered titles
+        if (response.page < response.total_pages) {
+          getTitles(substr, pageNumber + 1, titles);
+        } else {
+          return titles;
+        }
       });
-
-
     }).on('error', (e) => {
       console.error(e);
     });
-
   };
 
 
-  getTitles(substr, 1);
-}
+  getTitles(substr, 1, []);
+};
 
 getMovieTitles('spiderman');
