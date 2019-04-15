ae31329a16b885f26026214efdfedad9a03851f3
Ron Arbel <ronarbel1@gmail.com>
2019-04-15 15:41:02 -0700

    Complete naive solution

--- realInterviewQuestions/V/doesSumExist.js
+++ realInterviewQuestions/V/doesSumExist.js
@@ -1,10 +1,16 @@
 // Given a sum and an array of integers, write an algorithm that 
 // determines if there are 2 numbers in the array that add up to the given sum.
 
+// naïve solution
 const doesSumExist = (arr, target) => {
-  const compliments = arr.map(v => target - v);
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = i + 1; j < arr.length; j += 1) {
+      if (arr[i] + arr[j] === target) return true;
+    }
+  }
+  return false;
 };
 
 console.log(doesSumExist([1, 2, 3, 4], 7)); // true
-console.log(doesSumExist([3, 5, 6, 7], 10)); // false
+console.log(doesSumExist([5, 6, 7], 10)); // false
 console.log(doesSumExist([5, 5, 6, 7], 10)); // true
