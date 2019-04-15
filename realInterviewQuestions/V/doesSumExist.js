bd5500d04a9ee7e7e5f591cda3e97f541f0fb4a2
Ron Arbel <ronarbel1@gmail.com>
2019-04-15 15:47:04 -0700

    Complete solution

--- realInterviewQuestions/V/doesSumExist.js
+++ realInterviewQuestions/V/doesSumExist.js
@@ -1,12 +1,24 @@
-// Given a sum and an array of integers, write an algorithm that 
+// Given a sum and an array of integers, write an algorithm that
 // determines if there are 2 numbers in the array that add up to the given sum.
 
-// naïve solution
+// quadratic time, constant space
+// const doesSumExist = (arr, target) => {
+//   for (let i = 0; i < arr.length; i += 1) {
+//     for (let j = i + 1; j < arr.length; j += 1) {
+//       if (arr[i] + arr[j] === target) return true;
+//     }
+//   }
+//   return false;
+// };
+
+// linear time, linear space
 const doesSumExist = (arr, target) => {
+  const differences = new Set();
   for (let i = 0; i < arr.length; i += 1) {
-    for (let j = i + 1; j < arr.length; j += 1) {
-      if (arr[i] + arr[j] === target) return true;
+    if (differences.has(arr[i])) {
+      return true;
     }
+    differences.add(target - arr[i]);
   }
   return false;
 };
