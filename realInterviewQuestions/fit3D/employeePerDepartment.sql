130457144956a3d83ccebdbfe7c31cf5fecfca11
Ron Arbel <ronarbel1@gmail.com>
2019-04-26 19:36:02 -0700

    Updated query samples

--- realInterviewQuestions/fit3D/employeePerDepartment.sql
+++ realInterviewQuestions/fit3D/employeePerDepartment.sql
@@ -3,3 +3,16 @@ FROM department
 INNER JOIN employee, SUM(COUNT_OF_EMPLOYEES_IN_THE_DEPARTMENT), 
 
 
+SELECT (DISTINCT?) department.name, sum_of_emp
+FROM department
+	LEFT JOIN employee
+		ON id=dept_id
+ORDER BY sum_of_emp DESC
+
+https://sqlbolt.com/lesson/select_queries_with_aggregates
+
+SELECT name, count(*) AS count_of_employees_in_the_department
+FROM department
+	LEFT JOIN employee
+		ON id=dept_id
+ORDER BY count_of_employees_in_the_department DESC
