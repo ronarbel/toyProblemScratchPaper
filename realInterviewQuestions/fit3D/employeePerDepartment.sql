b2c5279cb5d7c17085041304d865165119539cb8
Ron Arbel <ronarbel1@gmail.com>
2019-04-30 23:57:27 -0700

    Update solution

--- realInterviewQuestions/fit3D/employeePerDepartment.sql
+++ realInterviewQuestions/fit3D/employeePerDepartment.sql
@@ -11,8 +11,9 @@ ORDER BY sum_of_emp DESC
 
 https://sqlbolt.com/lesson/select_queries_with_aggregates
 
-SELECT name, count(*) AS count_of_employees_in_the_department
-FROM department
+SELECT department.name, count(employee.id) AS count_of_employees_in_the_department
+FROM department.name
 	LEFT JOIN employee
-		ON id=dept_id
-ORDER BY count_of_employees_in_the_department DESC
+		ON department.id=dept_id
+GROUP BY department.name
+ORDER BY count_of_employees_in_the_department DESC, department.name;
--- realInterviewQuestions/fit3D/movieTitles2.js
+++ realInterviewQuestions/fit3D/movieTitles2.js
@@ -1,36 +1,38 @@
+/* eslint-disable */
 const https = require('https');
 
-const getMovieTitles = (substr) => {
+function getMovieTitles(substr) {
   const getTitles = (substr, pageNumber, titles) => {
-    https.get(`https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=${pageNumber}`, (res) => {
-      // console.log('statusCode:', res.statusCode);
-      // console.log('headers:', res.headers);
-      res.on('data', (chunk) => {
-        const response = JSON.parse(chunk);
+      https.get(`https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=${pageNumber}`, (res) => {
+          // console.log('statusCode:', res.statusCode);
+          // console.log('headers:', res.headers);
+          res.on('data', (chunk) => {
+              const response = JSON.parse(chunk);
 
-        // add retrieved titles to titles array.
-        response.data.forEach((movie) => {
-          titles.push(movie.Title);
-        });
+              // add retrieved titles to titles array.
+              response.data.forEach((movie) => {
+                  titles.push(movie.Title);
+              });
 
-        // retrieve next page if more pages, else return gathered titles
-        if (response.page < response.total_pages) {
-          getTitles(substr, pageNumber + 1, titles);
-        } else {
-          titles.sort((a, b) => {
-            if (a < b) return -1;
-            if (b < a) return 1;
-            return 0;
+              // retrieve next page if more pages, else return gathered titles
+              if (response.page < response.total_pages) {
+                  getTitles(substr, pageNumber + 1, titles);
+              } else {
+                  titles.sort((a, b) => {
+                      if (a < b) return -1;
+                      if (b < a) return 1;
+                      return 0;
+                  });
+                  
+                  titles.forEach(title => console.log(title));
+              }
           });
-          console.log(titles);
-        }
+      }).on('error', (e) => {
+          console.error(e);
       });
-    }).on('error', (e) => {
-      console.error(e);
-    });
   };
 
   getTitles(substr, 1, []);
-};
+}
 
 getMovieTitles('spiderman');
